495f3294541abcd5f40f31f0d9d09ed0cc82013b
Jonathan Rivera <jrivera@i-4web.com>
2013-08-01 23:02:15 -0400

    Updated Front End
    -Changed The Endorse Heads to display as unordered list.
    -Added boolean parameter to get_business_name
    -Added 'no_endorse_head_display' to handle the Endorsers that don't
    provide images or don't want to use images.

--- frontend/class-endorse-heads.php
+++ frontend/class-endorse-heads.php
@@ -35,7 +35,8 @@ class Endorse_Heads_Frontend {
 		$query = new WP_Query( $args );
 
 		// The Loop
-
+		echo '<ul class="thumbnails">';
+		
 		if ($query->have_posts()) :  //Make sure there are Endorse Heads
 		
 		while ( $query->have_posts() ) : $query->the_post(); 
@@ -43,7 +44,8 @@ class Endorse_Heads_Frontend {
 			global $postID;  //Setup Global Variable for Post ID
 			$postID = get_the_ID(); //grab the Post ID
 			
-			echo '<div class="endorse-head-wrap span2">';
+			echo '<li class="span2">';
+			echo '<div class="endorse-head-wrap">';
 		
 			//Display The Endorse Head Pic
 			$this->endorse_head_pic();
@@ -55,12 +57,13 @@ class Endorse_Heads_Frontend {
 			//$this->get_endorser_title();
 			
 			//Display The Business Name
-			$this->get_business_name();
+			$this->get_business_name(false);
 			
 			//Display The Social Media Links
 			$this->get_quote();
 			
 			echo '</div>';
+			echo '</li>';
 			
 		endwhile;
 		
@@ -68,7 +71,7 @@ class Endorse_Heads_Frontend {
 				echo 'There are no Endorse Heads at this time :( ';
 				
 			endif;
-		
+		echo '</ul>';
 		/* Restore original Post Data 
 		 */
 		wp_reset_postdata();
@@ -105,14 +108,18 @@ class Endorse_Heads_Frontend {
 	 *
 	 * @since 	  1.0.0
 	 */
-	private function get_business_name(){
+	private function get_business_name($removeClass = false){
 		
 		global $postID, $prefix;
 		
 		$key = $prefix.'business_name';
 		$businessName = get_post_meta( $postID, $key, true );
 		
+		if (!$removeClass){
 		echo '<h6 class="endorser-business"><small>'.$businessName .'</small><h6>';
+		}
+		else
+			echo '<h6>'.$businessName.'</h6>';
 	}
 	
 	 /**
@@ -130,6 +137,68 @@ class Endorse_Heads_Frontend {
 
 		echo '<p class="endorser-quote">"</span>'.$endorserQuote .'"</p>';		 
 	 }
+	 
+	 /**
+	 * The Boring Endorser Loop :)
+	 *
+	 * @since 	  1.0.0
+	 */
+	  public function non_endorse_heads_loop() {
+		  
+		  	//Setup the args
+		$args_no_heads = array(
+			'post_type' => 'tennis_endorsement',  //query the endorsements post type
+			'meta_key' => 'i4_endorser_display',  //query the Endorser Heads
+			'meta_value' => 'Normal Listing'
+		);
+		$query_no_heads = new WP_Query( $args_no_heads );
+		
+		// The Loop
+		echo '<table class="table table-striped">';
+		
+		echo '<thead>
+			  <tr>
+			   <td>Name</td>
+			   <td>Business</td>';
+		echo '</tr>
+			  </thead>';
+		
+		echo '<tbody>';
+		
+		if ($query_no_heads->have_posts()) :  //Make sure there are Endorse Heads
+		
+		while ( $query_no_heads->have_posts() ) : $query_no_heads->the_post(); 
+			
+			global $postID;  //Setup Global Variable for Post ID
+			$postID = get_the_ID(); //grab the Post ID
+			
+
+					
+			//Display The Endorsers Name
+			echo '<tr>
+				  <td><b>' .get_the_title() .'</b> </td>
+				  <td>';
+			echo $this->get_business_name(true);
+			echo '</td>
+				  </tr>';
+			//Display The Endorsers Title
+			//$this->get_endorser_title();
+			
+						
+		endwhile;
+		
+			else:  //No Endorse Heads...Sad Face.
+				echo 'There are no Endorse Heads at this time :( ';
+				
+			endif;
+		echo '</tbody>
+     		  </table>';
+		/* Restore original Post Data 
+		 */
+		wp_reset_postdata();		
+		  
+	  }
+	 
 	
 
 }
@@ -151,9 +220,26 @@ class Endorse_Heads_Frontend {
 		function endorse_heads_display( ) {
 			global $i4_endorse_heads;
 			
-			return $i4_endorse_heads->endorse_heads_loop( $requestType );
+			return $i4_endorse_heads->endorse_heads_loop();
 		}
 	}
+	
+	if ( !function_exists( 'non_endorse_heads_display' ) ) {
+	
+		/**
+		 * Template tag for displaying the Endorsement Listing With No Pictures.
+		 *
+		 * @param string $before  What to show before the breadcrumb.
+		 * @param string $after   What to show after the breadcrumb.
+		 * @param bool   $display Whether to display the breadcrumb (true) or return it (false).
+		 * @return string
+		 */
+		function non_endorse_heads_display( ) {
+			global $i4_endorse_heads;
+			
+			return $i4_endorse_heads->non_endorse_heads_loop();
+		}
+	}	
 
 
 ?>
